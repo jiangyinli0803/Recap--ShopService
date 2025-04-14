0582c688cfd27e8d69b938984abfabb0184b740c
Yinli <jiangyinli0803@gmail.com>
2025-04-14 17:05:07 +0200

    change timeStamp type to Instant

--- src/test/java/ShopServiceTest.java
+++ src/test/java/ShopServiceTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.time.Instant;
+
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,7 +18,7 @@ class ShopServiceTest {
         Order actual = shopService.addOrder(productsIds);
 
         //THEN
-        Order expected = new Order("-1", List.of(new Product("1", "Apfel")), OrderStatus.PROCESSING);
+        Order expected = new Order("-1", List.of(new Product("1", "Apfel")), OrderStatus.PROCESSING, Instant.now());
         assertEquals(expected.products(), actual.products());
         assertNotNull(expected.id());
     }
